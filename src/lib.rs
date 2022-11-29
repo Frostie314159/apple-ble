ba28089165291d18ec7b35b33ff341190ff4132a
Frostie314159 <iam.an.programmer@gmail.com>
2022-11-29 17:31:34 +0100

    Removed warnings and clean up.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(incomplete_features)]
 // opt-out of using the unstable feature "async_fn_in_trait". See https://github.com/rust-lang/rust/issues/91611.
 #![cfg_attr(not(feature = "disable_afit"), feature(async_fn_in_trait))]
 mod util;
--- tests/airdrop_advertisement.rs
+++ tests/airdrop_advertisement.rs
@@ -4,9 +4,9 @@ use tokio::test;
 
 #[test(flavor = "multi_thread", worker_threads = 1)]
 async fn register_airdrop_advertisement() -> Result<(), Box<dyn Error>> {
-    let session = apple_ble::session::Session::new().await;
+    let session = apple_ble::session::Session::new().await?;
     apple_ble::advertisements::AirDropAdvertisement::register(
-        &session.unwrap(),
+        &session,
         &apple_ble::advertisements::AirDropAdvertisementData::new(
             None, None, None,
         ),
