a1114e8b486c77cd127684eeb12dad585844929e
Frostie314159 <iam.an.programmer@gmail.com>
2022-12-07 17:20:27 +0100

    Implemented From<> trait.

--- src/advertisement.rs
+++ src/advertisement.rs
@@ -1,16 +1,19 @@
+use std::fmt::Debug;
 use std::net::{Ipv4Addr, Ipv6Addr};
 use std::{collections::BTreeMap, error::Error, time::Duration};
 
 #[cfg(feature = "disable_afit")]
 use async_trait::async_trait;
 use bluer::adv::{Advertisement, AdvertisementHandle, Type};
+use bluer::{Device, Address};
+use futures::executor;
 
 use crate::session::Session;
-use crate::util::{get_first_two_bytes_of_sha256, set_device_addr};
+use crate::util::set_device_addr;
 
 const APPLE_MAGIC: u16 = 0x4C00_u16;
 
-pub trait AdvertisableData: Clone {
+pub trait AdvertisableData: Clone + PartialEq + Debug {
     fn octets(&self) -> Vec<u8>;
 }
 
@@ -34,39 +37,69 @@ pub trait Advertisable<T: AdvertisableData> {
     ) -> Result<AdvertisementHandle, Box<dyn Error>> {
         Self::validate_user_data(user_data)?;
         let advertisement = Self::assemble_advertisement(session, user_data)?;
-        Ok(session
-            .adapter
-            .advertise(advertisement)
-            .await?)
+        Ok(session.adapter.advertise(advertisement).await?)
+    }
+}
+pub enum AdvertisementType {
+    AirDrop(AirDropAdvertisementData),
+    AirPlaySource, // Carries no dynamic data.
+    AirPlayTarget(AirPlayTargetAdvertisementData),
+    AirPrint(AirPrintAdvertisementData),
+    FindMy(FindMyAdvertisementData),
+}
+pub fn get_adv_data_from_device(device: Device) -> Option<AdvertisementType> {
+    let binding = executor::block_on(device.manufacturer_data()).ok()??;
+    let manufacturer_data = binding.get(&APPLE_MAGIC)?;
+    match manufacturer_data[0] {
+        0x05 => Some(AdvertisementType::AirDrop(
+            AirDropAdvertisementData::try_from(manufacturer_data.clone()).ok()?,
+        )),
+        0x0a => Some(AdvertisementType::AirPlaySource),
+        0x09 => Some(AdvertisementType::AirPlayTarget(
+            AirPlayTargetAdvertisementData::try_from(manufacturer_data.clone()).ok()?,
+        )),
+        0x03 => Some(AdvertisementType::AirPrint(
+            AirPrintAdvertisementData::try_from(manufacturer_data.clone()).ok()?,
+        )),
+        0x12 => Some(AdvertisementType::FindMy(
+            FindMyAdvertisementData::try_from((device.address(), manufacturer_data.clone())).ok()?,
+        )),
+        _ => None,
     }
 }
 
-
 /// Data for an AirDrop advertisement.
-#[derive(Clone)]
+#[derive(Clone, PartialEq, Debug)]
 pub struct AirDropAdvertisementData {
-    pub apple_id: Option<String>,
-    pub phone: Option<String>,
-    pub email: Option<String>
+    pub apple_id: [u8; 2],
+    pub phone: [u8; 2],
+    pub email: [u8; 2],
 }
 impl AdvertisableData for AirDropAdvertisementData {
     fn octets(&self) -> Vec<u8> {
-        let apple_id = get_first_two_bytes_of_sha256(self.apple_id.clone().unwrap_or_default());
-        let phone = get_first_two_bytes_of_sha256(self.phone.clone().unwrap_or_default());
-        let email = get_first_two_bytes_of_sha256(self.email.clone().unwrap_or_default());
-        [   
-            
+        [
             vec![
-                0x05,   // Message type
-                0x12    // Message length
+                0x05, // Message type
+                0x12, // Message length
             ],
-            vec![0;8],  // 8bytes of padding
+            vec![0; 8], // 8bytes of padding
             vec![0x01], // AirDrop version
-            apple_id.to_vec(),
-            phone.to_vec(),
-            email.to_vec(),
-            email.to_vec()
-        ].concat()
+            self.apple_id.to_vec(),
+            self.phone.to_vec(),
+            self.email.to_vec(),
+            self.email.to_vec(),
+        ]
+        .concat()
+    }
+}
+impl TryFrom<Vec<u8>> for AirDropAdvertisementData {
+    type Error = Box<dyn Error>;
+    fn try_from(value: Vec<u8>) -> Result<Self, Self::Error> {
+        Ok(AirDropAdvertisementData {
+            apple_id: value[11..13].try_into()?,
+            phone: value[13..15].try_into()?,
+            email: value[15..17].try_into()?,
+        })
     }
 }
 
@@ -83,27 +116,29 @@ impl Advertisable<AirDropAdvertisementData> for AirDropAdvertisement {
             timeout: Some(Duration::from_millis(0)),
             min_interval: Some(Duration::from_millis(100)),
             max_interval: Some(Duration::from_millis(200)),
-            manufacturer_data: BTreeMap::from([(
-                APPLE_MAGIC,
-                user_data.octets(),
-            )]),
+            manufacturer_data: BTreeMap::from([(APPLE_MAGIC, user_data.octets())]),
             ..Default::default()
         })
     }
 }
 
-
 /// Data for an AirPlay source message
-#[derive(Clone)]
+#[derive(Clone, PartialEq, Debug)]
 pub struct AirPlaySourceAdvertisementData;
 impl AdvertisableData for AirPlaySourceAdvertisementData {
     fn octets(&self) -> Vec<u8> {
         // This is constant.
         vec![
-            0x0a,   // Message type
-            0x01,   // Message length
-            0x00
+            0x0a, // Message type
+            0x01, // Message length
+            0x00,
         ]
+    }
+}
+impl TryFrom<Vec<u8>> for AirPlaySourceAdvertisementData {
+    type Error = Box<dyn Error>;
+    fn try_from(_value: Vec<u8>) -> Result<Self, Self::Error> {
+        Ok(AirPlaySourceAdvertisementData {})
     }
 }
 
@@ -115,43 +150,44 @@ impl Advertisable<AirPlaySourceAdvertisementData> for AirPlaySourceAdvertisement
         session: &mut Session,
         user_data: &AirPlaySourceAdvertisementData,
     ) -> Result<Advertisement, Box<dyn Error>> {
-        Ok(Advertisement{
+        Ok(Advertisement {
             advertisement_type: Type::Broadcast,
             local_name: Some(session.adapter.name().to_string()),
             timeout: Some(Duration::from_millis(0)),
             min_interval: Some(Duration::from_millis(100)),
             max_interval: Some(Duration::from_millis(200)),
-            manufacturer_data: BTreeMap::from([(
-                APPLE_MAGIC,
-                user_data.octets()
-            )]),
+            manufacturer_data: BTreeMap::from([(APPLE_MAGIC, user_data.octets())]),
             ..Default::default()
         })
     }
 }
 
-
 /// Data for an AirPlay target message
-#[derive(Clone)]
+#[derive(Clone, PartialEq, Debug)]
 pub struct AirPlayTargetAdvertisementData {
-    pub flags: Option<u8>,
-    pub seed: Option<u8>,
-    pub ip_address: Ipv4Addr
+    pub ip_address: Ipv4Addr,
 }
 impl AdvertisableData for AirPlayTargetAdvertisementData {
     fn octets(&self) -> Vec<u8> {
-        let flags = self.flags.unwrap_or(0x03);
-        let seed = self.seed.unwrap_or(0x07);
         let ip_address = self.ip_address.octets();
         [
             vec![
-                0x09,   // Message type
-                0x06,   // Message length
-                flags,
-                seed
+                0x09, // Message type
+                0x06, // Message length
+                0x03, 0x07,
             ],
-            ip_address.to_vec()
-        ].concat()
+            ip_address.to_vec(),
+        ]
+        .concat()
+    }
+}
+impl TryFrom<Vec<u8>> for AirPlayTargetAdvertisementData {
+    type Error = Box<dyn Error>;
+    fn try_from(value: Vec<u8>) -> Result<Self, Self::Error> {
+        let ip_address: [u8; 4] = value[4..8].try_into()?;
+        Ok(AirPlayTargetAdvertisementData {
+            ip_address: Ipv4Addr::from(ip_address),
+        })
     }
 }
 
@@ -162,28 +198,24 @@ impl Advertisable<AirPlayTargetAdvertisementData> for AirPlayTargetAdvertisement
         session: &mut Session,
         user_data: &AirPlayTargetAdvertisementData,
     ) -> Result<Advertisement, Box<dyn Error>> {
-        Ok(Advertisement{
+        Ok(Advertisement {
             advertisement_type: Type::Broadcast,
             local_name: Some(session.adapter.name().to_string()),
             timeout: Some(Duration::from_millis(0)),
             min_interval: Some(Duration::from_millis(100)),
             max_interval: Some(Duration::from_millis(200)),
-            manufacturer_data: BTreeMap::from([(
-                APPLE_MAGIC,
-                user_data.octets()
-            )]),
+            manufacturer_data: BTreeMap::from([(APPLE_MAGIC, user_data.octets())]),
             ..Default::default()
         })
     }
 }
 
-
 /// Data for an AirPrint message
-#[derive(Clone)]
+#[derive(Clone, PartialEq, Debug)]
 pub struct AirPrintAdvertisementData {
     pub port: u16,
     pub ip_addr: Ipv6Addr,
-    pub power: u8
+    pub power: u8,
 }
 impl AdvertisableData for AirPrintAdvertisementData {
     fn octets(&self) -> Vec<u8> {
@@ -191,18 +223,28 @@ impl AdvertisableData for AirPrintAdvertisementData {
         let ip_addr = self.ip_addr.octets();
         [
             vec![
-                0x03,   // Message type
-                0x16,   // Message length
-                0x74,   // Address type
-                0x07,   // Resource path
-                0x6f    // Security type
+                0x03, // Message type
+                0x16, // Message length
+                0x74, // Address type
+                0x07, // Resource path
+                0x6f, // Security type
             ],
             port.to_vec(),
             ip_addr.to_vec(),
-            vec![
-                self.power
-            ]
-        ].concat()
+            vec![self.power],
+        ]
+        .concat()
+    }
+}
+impl TryFrom<Vec<u8>> for AirPrintAdvertisementData {
+    type Error = Box<dyn Error>;
+    fn try_from(value: Vec<u8>) -> Result<Self, Self::Error> {
+        let ip_address: [u8; 16] = value[7..23].try_into()?;
+        Ok(AirPrintAdvertisementData {
+            port: (value[5] as u16) << 8 | value[6] as u16,
+            ip_addr: Ipv6Addr::from(ip_address),
+            power: value[23],
+        })
     }
 }
 
@@ -213,42 +255,48 @@ impl Advertisable<AirPrintAdvertisementData> for AirPrintAdvertisement {
         session: &mut Session,
         user_data: &AirPrintAdvertisementData,
     ) -> Result<Advertisement, Box<dyn Error>> {
-        Ok(Advertisement{
+        Ok(Advertisement {
             advertisement_type: Type::Broadcast,
             local_name: Some(session.adapter.name().to_string()),
             timeout: Some(Duration::from_millis(0)),
             min_interval: Some(Duration::from_millis(100)),
             max_interval: Some(Duration::from_millis(200)),
-            manufacturer_data: BTreeMap::from([(
-                APPLE_MAGIC,
-                user_data.octets()
-            )]),
+            manufacturer_data: BTreeMap::from([(APPLE_MAGIC, user_data.octets())]),
             ..Default::default()
         })
     }
 }
 
-
 /// Data for a FindMy message
-#[derive(Clone)]
+#[derive(Clone, PartialEq, Debug)]
 pub struct FindMyAdvertisementData {
-    pub public_key: [u8; 28]
+    pub public_key: [u8; 28],
 }
 impl AdvertisableData for FindMyAdvertisementData {
     fn octets(&self) -> Vec<u8> {
         let public_key = self.public_key.split_at(6);
         [
             vec![
-                0x12,   // Message length
-                0x19,   // Message type
-                0x00
+                0x12, // Message type
+                0x19, // Message length
+                0x00,
             ],
             public_key.1.to_vec(),
-            vec![
-                public_key.0[0] >> 6,
-                0x00
-            ]
-        ].concat()
+            vec![public_key.0[0] >> 6],
+        ]
+        .concat()
+    }
+}
+impl TryFrom<(Address, Vec<u8>)> for FindMyAdvertisementData {
+    type Error = Box<dyn Error>;
+    fn try_from(value: (Address, Vec<u8>)) -> Result<Self, Self::Error> {
+        let public_key: [u8; 28] = [&value.0.0, &value.1[2..24]]
+            .concat()
+            .as_slice()
+            .try_into()?;
+        Ok(FindMyAdvertisementData {
+            public_key: public_key,
+        })
     }
 }
 
@@ -260,16 +308,13 @@ impl Advertisable<FindMyAdvertisementData> for FindMyAdvertisement {
         user_data: &FindMyAdvertisementData,
     ) -> Result<Advertisement, Box<dyn Error>> {
         set_device_addr(session, &user_data.public_key[0..6])?;
-        Ok(Advertisement{
+        Ok(Advertisement {
             advertisement_type: Type::Broadcast,
             local_name: Some(session.adapter.name().to_string()),
             timeout: Some(Duration::from_millis(0)),
             min_interval: Some(Duration::from_millis(100)),
             max_interval: Some(Duration::from_millis(200)),
-            manufacturer_data: BTreeMap::from([(
-                APPLE_MAGIC,
-                user_data.octets()
-            )]),
+            manufacturer_data: BTreeMap::from([(APPLE_MAGIC, user_data.octets())]),
             ..Default::default()
         })
     }
--- tests/advertisements.rs
+++ tests/advertisements.rs
@@ -1,4 +1,5 @@
-use apple_ble::advertisement::Advertisable;
+use apple_ble::advertisement::{Advertisable, AirDropAdvertisementData, AdvertisableData, AirPlayTargetAdvertisementData, AirPrintAdvertisementData, FindMyAdvertisementData};
+use bluer::Address;
 use std::{error::Error, net::{Ipv4Addr, Ipv6Addr}};
 use tokio::test;
 
@@ -8,9 +9,9 @@ async fn test_airdrop_advertisement() -> Result<(), Box<dyn Error>> {
     apple_ble::advertisement::AirDropAdvertisement::register(
         &mut session,
         &apple_ble::advertisement::AirDropAdvertisementData{
-            apple_id: None,
-            phone: None,
-            email : None
+            apple_id: [0x00, 0x00],
+            phone: [0x00, 0x00],
+            email : [0x00, 0x00]
         },
     )
     .await?;
@@ -34,9 +35,7 @@ async fn test_airplaytarget_advertisement() -> Result<(), Box<dyn Error>> {
     apple_ble::advertisement::AirPlayTargetAdvertisement::register(
         &mut session,
         &apple_ble::advertisement::AirPlayTargetAdvertisementData {
-            flags: Some(0x0),
-            seed: Some(0x42),
-            ip_address: Ipv4Addr::from([133, 71, 33, 7])
+            ip_address: Ipv4Addr::LOCALHOST
         },
     )
     .await?;
@@ -64,9 +63,45 @@ async fn test_findmy_advertisement() -> Result<(), Box<dyn Error>> {
     apple_ble::advertisement::FindMyAdvertisement::register(
         &mut session,
         &apple_ble::advertisement::FindMyAdvertisementData {
-            public_key: [0; 28]
+            public_key: [0x88; 28]
         },
     )
     .await?;
+    Ok(())
+}
+
+#[test(flavor = "multi_thread", worker_threads = 1)]
+async fn test_serialization_and_deserialization() -> Result<(), Box<dyn Error>> {
+    let data = AirDropAdvertisementData {
+        apple_id: [0xfe, 0xdc],
+        email: [0xba, 0x98],
+        phone: [0x76, 0x54]
+    };
+    let serialized = data.clone().octets();
+    let deserialized = AirDropAdvertisementData::try_from(serialized)?;
+    assert_eq!(data, deserialized);
+
+    let data = AirPlayTargetAdvertisementData {
+        ip_address: Ipv4Addr::LOCALHOST
+    };
+    let serialized = data.clone().octets();
+    let deserialized = AirPlayTargetAdvertisementData::try_from(serialized)?;
+    assert_eq!(data, deserialized);
+
+    let data = AirPrintAdvertisementData {
+        port: 0xf00d,
+        ip_addr: Ipv6Addr::LOCALHOST,
+        power: 0xff
+    };
+    let serialized = data.clone().octets();
+    let deserialized = AirPrintAdvertisementData::try_from(serialized)?;
+    assert_eq!(data, deserialized);
+
+    let data = FindMyAdvertisementData {
+        public_key: [0x00_u8; 28]
+    };
+    let serialized = data.clone().octets();
+    let deserialized = FindMyAdvertisementData::try_from((Address::new(data.public_key[0..6].try_into()?), serialized))?;
+    assert_eq!(data, deserialized);
     Ok(())
 }
